b301fab0dc67f21887715b741a537e5f81ae3666
Weigo <37270925+weigo87@users.noreply.github.com>
2024-12-21 18:15:55 +0100

    Adario NPC

---
+++ TDD/_Adario/D/ADARIOP.D
@@ -0,0 +1,143 @@
+BEGIN ~ADARIOP~
+
+IF ~Global("KickedOut","LOCALS",0)~ THEN BEGIN 0
+  SAY @0
+  IF ~~ THEN REPLY @1 GOTO 1
+  IF ~~ THEN REPLY @2 DO ~JoinParty()~ EXIT
+END
+
+IF ~~ THEN BEGIN 1 // from: 0.0
+  SAY @3
+  IF ~!AreaCheck("AR0602")
+!AreaCheck("AR0603")
+!AreaCheck("AR0410")
+!AreaCheck("AR0411")
+!AreaCheck("AR0412")
+!AreaCheck("AR0413")
+!AreaCheck("AR0414")
+!AreaCheck("AR1512")
+!AreaCheck("AR1513")
+!AreaCheck("AR1514")
+!AreaCheck("AR1515")
+!AreaCheck("AR1516")
+!AreaCheck("AR1501")
+!AreaCheck("AR1502")
+!AreaCheck("AR1503")
+!AreaCheck("AR1504")
+!AreaCheck("AR1505")
+!AreaCheck("AR1506")
+!AreaCheck("AR1507")
+!AreaCheck("AR1508")
+!AreaCheck("AR1509")
+!AreaCheck("AR1510")
+!AreaCheck("AR1511")
+!AreaCheck("AR2100")
+!AreaCheck("AR2101")
+!AreaCheck("AR2102")
+!AreaCheck("AR2200")
+!AreaCheck("AR2202")
+!AreaCheck("AR2203")
+!AreaCheck("AR0602")
+!AreaCheck("AR0603")
+!AreaCheck("AR0410")
+!AreaCheck("AR0411")
+!AreaCheck("AR0412")
+!AreaCheck("AR0413")
+!AreaCheck("AR0414")
+!AreaCheck("AR1512")
+!AreaCheck("AR1513")
+!AreaCheck("AR1514")
+!AreaCheck("AR1515")
+!AreaCheck("AR1516")
+!AreaCheck("AR1501")
+!AreaCheck("AR1502")
+!AreaCheck("AR1503")
+!AreaCheck("AR1504")
+!AreaCheck("AR1505")
+!AreaCheck("AR1506")
+!AreaCheck("AR1507")
+!AreaCheck("AR1508")
+!AreaCheck("AR1509")
+!AreaCheck("AR1510")
+!AreaCheck("AR1511")
+!AreaCheck("AR2100")
+!AreaCheck("AR2101")
+!AreaCheck("AR2102")
+!AreaCheck("AR2200")
+!AreaCheck("AR2202")
+!AreaCheck("AR2203")
+!AreaCheck("AR0602")
+!AreaCheck("AR0603")
+!AreaCheck("AR0410")
+!AreaCheck("AR0411")
+!AreaCheck("AR0412")
+!AreaCheck("AR0413")
+!AreaCheck("AR0414")
+!AreaCheck("AR1512")
+!AreaCheck("AR1513")
+!AreaCheck("AR1514")
+!AreaCheck("AR1515")
+!AreaCheck("AR1516")
+!AreaCheck("AR1501")
+!AreaCheck("AR1502")
+!AreaCheck("AR1503")
+!AreaCheck("AR1504")
+!AreaCheck("AR1505")
+!AreaCheck("AR1506")
+!AreaCheck("AR1507")
+!AreaCheck("AR1508")
+!AreaCheck("AR1509")
+!AreaCheck("AR1510")
+!AreaCheck("AR1511")
+!AreaCheck("AR2100")
+!AreaCheck("AR2101")
+!AreaCheck("AR2102")
+!AreaCheck("AR2200")
+!AreaCheck("AR2202")
+!AreaCheck("AR2203")
+!AreaCheck("AR2204")
+!AreaCheck("AR2205")
+!AreaCheck("AR2206")
+!AreaCheck("AR2207")
+!AreaCheck("AR2208")
+!AreaCheck("AR2209")
+!AreaCheck("AR2210")
+!AreaCheck("AR2300")
+!AreaCheck("AR2400")
+!AreaCheck("AR2401")
+!AreaCheck("AR2402")
+!AreaCheck("AR2900")
+!AreaCheck("AR2901")
+!AreaCheck("AR2902")
+!AreaCheck("AR2903")
+!AreaCheck("AR2904")
+!AreaCheck("AR2905")
+!AreaCheck("AR2906")~ THEN REPLY @4 DO ~SetGlobal("KickedOut","LOCALS",1)
+LeaveParty()
+MoveGlobal("AR0406","ADARIO",[792.1527])~ EXIT
+  IF ~~ THEN REPLY @5 GOTO 2
+END
+
+IF ~~ THEN BEGIN 2 // from: 1.1
+  SAY @6
+  IF ~~ THEN DO ~SetGlobal("KickedOut","LOCALS",1)
+LeaveParty()~ EXIT
+END
+
+IF ~Global("KickedOut","LOCALS",1)~ THEN BEGIN 3
+  SAY @7
+  IF ~~ THEN REPLY @8 DO ~SetGlobal("KickedOut","LOCALS",0)
+JoinParty()~ EXIT
+  IF ~~ THEN REPLY @9 EXIT
+END
+
+IF ~Global("KickedOut","LOCALS",2)~ THEN BEGIN 4
+  SAY @10
+  IF ~~ THEN GOTO 5
+END
+
+IF ~~ THEN BEGIN 5 // from: 4.0
+  SAY @11
+  IF ~~ THEN DO ~LeaveParty()
+EscapeArea()~ EXIT
+END
---
+++ TDD/_Adario/D/KALVARAS.D
@@ -0,0 +1,100 @@
+BEGIN ~KALVARAS~
+
+IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
+  SAY @0
+  IF ~~ THEN REPLY @1 GOTO 1
+  IF ~~ THEN REPLY @2 GOTO 1
+END
+
+IF ~~ THEN BEGIN 1 // from: 0.1 0.0
+  SAY @3
+  IF ~~ THEN REPLY @4 GOTO 2
+END
+
+IF ~~ THEN BEGIN 2 // from: 1.0
+  SAY @5
+  IF ~~ THEN REPLY @6 GOTO 3
+  IF ~~ THEN REPLY @7 GOTO 11
+END
+
+IF ~~ THEN BEGIN 3 // from: 11.0 2.0
+  SAY @8
+  IF ~~ THEN REPLY @9 GOTO 4
+END
+
+IF ~~ THEN BEGIN 4 // from: 3.0
+  SAY @10
+  IF ~~ THEN REPLY @11 GOTO 5
+  IF ~~ THEN REPLY @12 DO ~SetGlobal("KickedOut","LOCALS",1)~ EXIT
+END
+
+IF ~~ THEN BEGIN 5 // from: 4.0
+  SAY @13
+  IF ~~ THEN REPLY @14 GOTO 6
+END
+
+IF ~~ THEN BEGIN 6 // from: 5.0
+  SAY @15
+  IF ~~ THEN REPLY @16 GOTO 7
+END
+
+IF ~~ THEN BEGIN 7 // from: 6.0
+  SAY @17
+  IF ~~ THEN REPLY @18 GOTO 8
+  IF ~~ THEN REPLY @19 GOTO 8
+END
+
+IF ~~ THEN BEGIN 8 // from: 7.1 7.0
+  SAY @20
+  IF ~~ THEN REPLY @21 GOTO 9
+  IF ~~ THEN REPLY @22 GOTO 10
+  IF ~~ THEN REPLY @23 DO ~JoinParty()~ EXIT
+END
+
+IF ~~ THEN BEGIN 9 // from: 8.0
+  SAY @24
+  IF ~~ THEN REPLY @23 DO ~JoinParty()~ EXIT
+END
+
+IF ~~ THEN BEGIN 10 // from: 8.1
+  SAY @25
+  IF ~~ THEN REPLY @23 DO ~JoinParty()~ EXIT
+END
+
+IF ~~ THEN BEGIN 11 // from: 2.1
+  SAY @26
+  IF ~~ THEN REPLY @27 GOTO 3
+END
+
+IF ~Global("KickedOut","LOCALS",1)~ THEN BEGIN 12
+  SAY @28
+  IF ~~ THEN REPLY @29 GOTO 13
+END
+
+IF ~~ THEN BEGIN 13 // from: 12.0
+  SAY @30
+  IF ~~ THEN REPLY @14 GOTO 14
+  IF ~~ THEN REPLY @31 EXIT
+END
+
+IF ~~ THEN BEGIN 14 // from: 13.0
+  SAY @32
+  IF ~~ THEN REPLY @33 GOTO 15
+END
+
+IF ~~ THEN BEGIN 15 // from: 14.0
+  SAY @34
+  IF ~~ THEN REPLY @21 GOTO 16
+  IF ~~ THEN REPLY @22 GOTO 17
+  IF ~~ THEN REPLY @23 DO ~JoinParty()~ EXIT
+END
+
+IF ~~ THEN BEGIN 16 // from: 15.0
+  SAY @24
+  IF ~~ THEN REPLY @23 DO ~JoinParty()~ EXIT
+END
+
+IF ~~ THEN BEGIN 17 // from: 15.1
+  SAY @25
+  IF ~~ THEN REPLY @23 DO ~JoinParty()~ EXIT
+END
